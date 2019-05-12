ff0ef909a7c28f1af5d346867eb6ca53e386757c
GemersonDenner <31903896+GemersonDenner@users.noreply.github.com>
2019-05-11 23:01:52 -0300

    adicionando swagger

--- KartApi.Api/Startup.cs
+++ KartApi.Api/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace KartApi.Api
 {
@@ -25,6 +27,10 @@ namespace KartApi.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Kart Api", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,6 +42,12 @@ namespace KartApi.Api
             }
 
             app.UseMvc();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Kart Api");
+            });
         }
     }
 }
